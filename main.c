98d39cc6c47ddbcbab1b3a32cd8235279f88f94a
Facundo Yuffrida <fyuffrida@baufest.com>
2018-10-12 16:28:05 -0300

    Primer commit: constante de matriz con su tamaño

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define cantidadFilas 8
+#define cantidadColumnas 13
+const char tabla_transiciones[cantidadFilas][cantidadColumnas] = {};
+
+int main(int argc, char *argv[]) {
+	char str[100];
+
+   printf("Ingresar cadena: ");
+   gets(str);
+
+   return 0;
+}
